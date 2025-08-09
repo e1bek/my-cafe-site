21b63ab85f356379ed140a84c8c81fc882cab723
e1bek <131474063+e1bek@users.noreply.github.com>
2025-08-09 12:41:09 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll", () => {
-    document.querySelectorAll(".about, .contact").forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const triggerPoint = window.innerHeight - 100;
-        if (sectionTop < triggerPoint) {
-            section.classList.add("visible");
-        }
-    });
-});
